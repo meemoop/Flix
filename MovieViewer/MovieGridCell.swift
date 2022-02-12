cb14e67412b1c4a60b696ebc13231ffd2d62b4d4
Melah Motani <melahmotani@Melahs-MacBook-Pro.local>
2022-02-12 15:08:13 -0500

    Completed Flix Part 2

---
+++ MovieViewer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  MovieViewer
+//
+//  Created by Melah Motani on 2/12/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
